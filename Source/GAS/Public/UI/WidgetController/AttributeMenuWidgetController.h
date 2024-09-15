4c2004e1ecd0bae1a7fa16caf87c72caac6940a4
reinhartz <the.reinhart@gmail.com>
2024-09-15 18:18:17 -0400

    Updated to 5.4 and added Multiplayer Sessions Plugin

--- Source/GAS/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/GAS/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -6,6 +6,10 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+class UAttributeInfo;
+struct FAuraAttributeInfo;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo&, Info);
+
 /**
  * 
  */
@@ -15,7 +19,15 @@ class GAS_API UAttributeMenuWidgetController : public UAuraWidgetController
 	GENERATED_BODY()
 
 public:
+
 	virtual void BindCallbacksToDependencies() override;
 	virtual void BroadcastInitialValues() override;
 	
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FAttributeInfoSignature AttributeInfoDelegate;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 };
